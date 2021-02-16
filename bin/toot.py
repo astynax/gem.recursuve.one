4292c5d4a3ed0f151291dd5f5679db1e6db3de84
Aleksei Pirogov <astynax@users.noreply.github.com>
2021-02-16 17:24:24 +0300

    add a script for micrologging

---
+++ bin/toot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from datetime import datetime
+import os
+import sys
+from pathlib import Path
+
+
+def main():
+    message = ' '.join(a.strip() for a in sys.argv[1:]).strip()
+    if not message:
+        print('Usage: toot.py some non-empty message')
+        sys.exit()
+
+    microlog = Path(sys.argv[0]).parent / '..' / 'data' / 'microlog.gmi'
+    if not microlog.is_file():
+        sys.exit(f'File {microlog} not found!')
+
+    stamp = datetime.now().strftime('%Y.%m.%d %H:%M')
+
+    buf = []
+    with microlog.open() as inp:
+        stream = iter(inp.readlines())
+        for line in stream:
+            if line.isspace():
+                break
+            buf.append(line)
+        buf.append(f'''
+### {stamp}
+
+{message}
+
+''')
+        for line in stream:
+            buf.append(line)
+
+    with microlog.open('w') as out:
+        out.writelines(buf)
+
+
+if __name__ == '__main__':
+    main()
